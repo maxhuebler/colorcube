a6519dbf20a059842bdc93bddc28f825271b3bde
Max Huebler <max@huebler.us>
2016-04-07 23:21:15 -0400

    Fixed getting stucked on the side of platforms, rewrote the PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour
     [HideInInspector] public bool jump = false;
 
     public float moveSpeed = 5f;
-    public float jumpForce = 500f;
+    public float jumpHeight = 500f;
 
     public Transform top_left;
     public Transform bottom_right;
@@ -21,26 +21,21 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        grounded = Physics2D.OverlapArea(top_left.position, bottom_right.position, ground);
         float x = Input.GetAxis("Horizontal") + Input.acceleration.x;
 
         rb.velocity = new Vector2(x * moveSpeed,rb.velocity.y);
 
-        if (jump)
-        {
-            rb.AddForce(new Vector2(0f, jumpForce));
-            jump = false;
-        }
-    }
-
-    void Update()
-    {
-        grounded = Physics2D.OverlapArea(top_left.position, bottom_right.position, ground);
-
-        if ((rb.velocity.y == 0f) && grounded)
+        if ((Mathf.Approximately(rb.velocity.y,0f) && grounded))
         {
             jump = true;
-            //Debug.Log("Grounded and jumped");
+            Debug.Log("Grounded and jumped");
         }
 
+        if (jump)
+        {
+            rb.AddForce(new Vector2(0f, jumpHeight));
+            jump = false;
+        }
     }
 }
